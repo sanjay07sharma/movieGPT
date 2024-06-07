6caa30dacc371822da056a42e0c12b841d37eede
Sanjay Sharma <sharma.sanjay813@gmail.com>
2024-06-07 22:49:59 +0530

    Setting up the initial components for netflix-gpt

---
+++ src/components/Body.js
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Browse from "./Browse"
+import Login from "./Login"
+
+const Body = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/browse" element={<Browse />} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default Body
+ 
