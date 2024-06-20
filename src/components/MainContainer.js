7e8885ed1ce1797019639aeca79244f081333f6f
Sanjay Sharma <sharma.sanjay813@gmail.com>
2024-06-20 22:40:40 +0530

    Video display setup, tmdb website not responding

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import { useSelector } from 'react-redux'
+import VideoTitle from './VideoTitle';
+import VideoBackground from './VideoBackground';
 
-const mainContainer = () => {
+
+const MainContainer = () => {
+  const movies = useSelector((store) => store.movies?.addNowPlayingMovie);
+  if (!movies) {
+    return;
+  }
+  const {original_title, overview} = movies;
+
   return (
     <div>
-
+      MainContainer
+      <VideoTitle title={[original_title, overview]}/>
+      <VideoBackground/>
     </div>
   )
 }
 
-export default mainContainer
+export default MainContainer
--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const secondaryContainer = () => {
   return (
     <div>
-
+      SecondaryContainer
     </div>
   )
 }
