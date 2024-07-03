d8e6bc129f110e67708e075cfac20a2f12ab147e
Sanjay Sharma <sharma.sanjay813@gmail.com>
2024-07-03 23:12:52 +0530

    Update GptSearch.js

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -1,9 +1,13 @@
 import GptMovieSuggestion from "./GptMovieSuggestion"
 import GptSearchBar from "./GptSearchBar"
+import { BG_URL } from "./utils/constants"
 
 const GptSearch = () => {
     return (
         <div>
+            <div className="absolute">
+              <img className="h-fit w-fit" src={BG_URL} alt="logo" />
+            </div>
             <GptSearchBar/>
             <GptMovieSuggestion/>
         </div>
